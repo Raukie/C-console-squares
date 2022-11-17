281c5e036e0293efb7e84ba9977b8f94ce120e9d
Raukie <90243019+Raukie@users.noreply.github.com>
2022-11-17 16:21:21 +0100

    Unused circle functions added, stopped flickering

--- c++/Source.cpp
+++ c++/Source.cpp
@@ -11,7 +11,7 @@
 struct Vector2 {
 	Vector2(int X, int Y) {
 		x = X;
-		y = Y; 
+		y = Y;
 	}
 	int x;
 	int y;
@@ -19,7 +19,7 @@ struct Vector2 {
 class util {
 public:
 	std::vector<Vector2> CalculateVertices(Vector2 start, Vector2 end) {
-		
+
 	}
 };
 
@@ -28,6 +28,7 @@ public:
 	square(Vector2 X, int w, int h, Vector2 DIR) : x(X), dir(DIR) {
 		W = w;
 		H = h;
+		radius = (x.x + w) / 2;
 		int rnd = std::rand() % 9;
 
 		switch (rnd) {
@@ -59,40 +60,65 @@ public:
 			Char = ']';
 			break;
 		}
-		
+
 	}
+	int rotation = 0;
 	char Char;
 	Vector2 x;
 	Vector2 dir;
 	int W, H;
-
+	int radius;
 	bool IsBetween(int min, int max, int value) {
 		bool t = false;
 		if (min > max) {
 			return value <= min && value >= max;
-		} else {
-			return value <= max&& value >= min;
+		}
+		else {
+			return value <= max && value >= min;
 		}
 	}
 
 	bool intersects(Vector2 point) {
+
+		return IsBetween(x.x, x.x + W, point.x) && IsBetween(x.y, x.y + H, point.y);
+	}
+
+	int DistanceToCenter(Vector2 point) {
+		if (rotation >= W + 2)
+			rotation = 0;
+		Vector2 center((x.x + x.x + W) / 2, (x.y + x.y + H) / 2);
+		int d = std::abs(center.x - point.x);
 		
-		return IsBetween(x.x, x.x+W, point.x) && IsBetween(x.y, x.y+H, point.y);
+		int t = std::abs(center.y - point.y);
+		rotation++;
+		return std::sqrt(d * d + t * t)-d+rotation;
+	}
+
+	bool inRange(Vector2 point) {
+		return DistanceToCenter(point) <= radius;
 	}
 
 	void OutOfBoundsCheck(int w, int h) {
-		if (!IsBetween(0, h, x.y + H+1) || !IsBetween(0, h, x.y)) {
-			dir.y = (-1)*dir.y;
+		if (!IsBetween(0, h, x.y + H + 1) || !IsBetween(0, h, x.y)) {
+			dir.y = (-1) * dir.y;
 		}
-		if (!IsBetween(0, w, x.x + W+1) || !IsBetween(0, w, x.x)) {
-			dir.x = (-1)*dir.x;
+		if (!IsBetween(0, w, x.x + W + 1) || !IsBetween(0, w, x.x)) {
+			dir.x = (-1) * dir.x;
 		}
 	}
-	
+
 };
 
 int clamp(int value, int max) {
 	return value > max ? max : value;
+}
+
+void setCursorPosition(int x, int y)
+{
+	static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	std::cout.flush();
+	COORD coord = { (SHORT)x, (SHORT)y };
+	SetConsoleCursorPosition(hOut, coord);
 }
 
 namespace drw {
@@ -153,7 +179,7 @@ namespace drw {
 					for (int i = 0; i < objects.size(); i++) {
 						if (objects[i].intersects(Vector2(x, y)))
 							line[x] = objects[i].Char;
-						
+
 					}
 				}
 			}
@@ -161,31 +187,36 @@ namespace drw {
 		}
 
 		void DrawFrame() {
+			std::string newFrame = "";
 			for (int y = 0; y < screenHeight; y++) {
 				std::string ln = "";
 				for (int x = 0; x < screenWidth; x++) {
-					 ln.push_back(frame[y][x]);
+					setCursorPosition(x, y);
+					ln.push_back(frame[y][x]);
 				}
-				std::cout << ln <<std::endl;
-				ln = "";
+				setCursorPosition(screenWidth, y);
+				ln.append("\n");
+				newFrame.append(ln);
 			}
+			setCursorPosition(0, 0);
+			std::cout << newFrame;
 		}
 
 		void GenerateRandomSquares(int amount) {
 			int w = 1;
 			int h = 1;
 			for (int i = 0; i < amount; i++) {
-				int speed = (std::rand() % 3)+1;
-				int dirx = std::rand() % 2 == 1 ? -1*speed : 1*speed;
-				int diry = std::rand() % 2 == 1 ? -1*speed : 1*speed;
+				int speed = (std::rand() % 3) + 1;
+				int dirx = std::rand() % 2 == 1 ? -1 * speed : 1 * speed;
+				int diry = std::rand() % 2 == 1 ? -1 * speed : 1 * speed;
 				int x = std::rand() % clamp(w, screenHeight);
 				int y = std::rand() % clamp(h, screenHeight);
 				square sqr = square(Vector2(x, y), std::rand() % clamp(20, screenWidth - w), std::rand() % clamp(20, screenHeight - h), Vector2(dirx, diry));
 				objects.push_back(sqr);
 				h += y;
 				x += w;
-					
-				
+
+
 			}
 		}
 	};
@@ -214,8 +245,8 @@ int main() {
 	std::cout << "How many random Objects: ";
 	std::cin >> input4;
 	pencil.GenerateRandomSquares(input4);
+	system("cls");
 	while (true) {
-		system("cls");
 		pencil.initFrame();
 		pencil.ProcessFrame();
 		pencil.DrawFrame();
